034b6fd0adc00e153b198e474f35277ed9bc6544
Debabrata Karfa <im@deb.im>
2024-07-12 00:26:31 +0530

    feat: Fix local

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,25 @@
+console.log(`Your port is ${process.env.PORT}`); // undefined
+const dotenv = require('dotenv');
+dotenv.config();
+console.log(`Your port is ${process.env.PORT}`); // 8626
+
 const express = require('express');
+const cors = require('cors');
 const databaseConfig = require('./config/database');
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
+const healthRoutes = require('./routes/healthRoutes');
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 databaseConfig.connect();
 
 app.use('/auth', authRoutes);
 app.use('/api', userRoutes);
+app.use('/health', healthRoutes);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
